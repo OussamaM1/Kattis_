3a1ba8e11c38dfbe7ed153dc5d017c67d24c45a4
oussamaM1 <makhloukoussama1@gmail.com>
2021-03-28 15:15:52 +0200

    SymmetricOrder

---
+++ Java/src/SymmetricOrder/SymmetricOrder.java
@@ -0,0 +1,134 @@
+package SymmetricOrder;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Problem solver = new Problem();
+        // int tt = in.nextInt();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Problem {
+        public void solve(int tt, InputReader in, OutputWriter out) {
+            int ans = 1;
+            while (true) {
+                int n = in.nextInt();
+                if (n == 0)
+                    return;
+                ArrayList<String> a = new ArrayList<>();
+                ArrayList<String> b = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    if (i % 2 == 0)
+                        a.add(in.nextLine());
+                    else
+                        b.add(in.nextLine());
+                }
+                out.println("SET " + ans);
+                for (int i = 0; i < a.size(); i++) {
+                    out.println(a.get(i));
+                }
+                for (int i = b.size() - 1; i >= 0; i--) {
+                    out.println(b.get(i));
+                }
+                ans++;
+            }
+
+        }
+
+    }
+
+    static class OutputWriter extends PrintWriter {
+        public OutputWriter(String fileName) throws FileNotFoundException {
+            super(fileName);
+        }
+
+        public OutputWriter(OutputStream outputStream) {
+            super(outputStream);
+        }
+
+        public OutputWriter(Writer writer) {
+            super(writer);
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader br;
+        StringTokenizer in;
+
+        public InputReader(String fileName) {
+            try {
+                br = new BufferedReader(new FileReader(fileName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public InputReader(InputStream inputStream) {
+            br = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        boolean hasMoreTokens() {
+            while (in == null || !in.hasMoreTokens()) {
+                String s = nextLine();
+                if (s == null) {
+                    return false;
+                }
+                in = new StringTokenizer(s);
+            }
+            return true;
+        }
+
+        public String nextString() {
+            return hasMoreTokens() ? in.nextToken() : null;
+        }
+
+        public String nextLine() {
+            try {
+                in = null;
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public int[][] nextIntIntArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+    }
+}
