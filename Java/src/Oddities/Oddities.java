a455e6bc53639d4cd517080e8940ea4d57ba8ec3
oussamaM1 <makhloukoussama1@gmail.com>
2021-04-01 15:21:22 +0100

    Oddities

---
+++ Java/src/Oddities/Oddities.java
@@ -0,0 +1,119 @@
+package Oddities;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        Problem solver = new Problem();
+        int tt = in.nextInt();
+        solver.solve(tt, in, out);
+        out.close();
+    }
+
+    static class Problem {
+        public void solve(int tt, InputReader in, OutputWriter out) {
+            while (tt-- > 0) {
+                int n = in.nextInt();
+                if (n % 2 == 0)
+                    out.println(n + " is even");
+                else
+                    out.println(n + " is odd");
+            }
+
+        }
+
+    }
+
+    static class OutputWriter extends PrintWriter {
+        public OutputWriter(String fileName) throws FileNotFoundException {
+            super(fileName);
+        }
+
+        public OutputWriter(OutputStream outputStream) {
+            super(outputStream);
+        }
+
+        public OutputWriter(Writer writer) {
+            super(writer);
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader br;
+        StringTokenizer in;
+
+        public InputReader(String fileName) {
+            try {
+                br = new BufferedReader(new FileReader(fileName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public InputReader(InputStream inputStream) {
+            br = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        boolean hasMoreTokens() {
+            while (in == null || !in.hasMoreTokens()) {
+                String s = nextLine();
+                if (s == null) {
+                    return false;
+                }
+                in = new StringTokenizer(s);
+            }
+            return true;
+        }
+
+        public String nextString() {
+            return hasMoreTokens() ? in.nextToken() : null;
+        }
+
+        public String nextLine() {
+            try {
+                in = null;
+                return br.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public int[][] nextIntIntArray(int n, int m) {
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = nextInt();
+                }
+            }
+            return a;
+        }
+
+    }
+}
